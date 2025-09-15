4185fb04b9cdc2ddeead7b36142b87f7febaf543
Arya Tabani <a.tabani82@gmail.com>
2025-09-15 23:31:00 +0330

    implementing user services

--- services/userService.go
+++ services/userService.go
@@ -1,1 +1,23 @@
 package services
+
+import (
+	"otp-login-service/models"
+	"otp-login-service/repository"
+)
+
+func GetUserByID(id int64) (*models.User, error) {
+	return repository.FindByID(id)
+}
+
+func ListUsers(page, limit int, search string) ([]*models.User, int) {
+	if page < 1 {
+		page = 1
+	}
+	if limit <= 0 {
+		limit = 10
+	}
+	return repository.FindAll(page, limit, search)
+}
+func GetUserByPhoneNumber(phone string) (*models.User, error) {
+	return repository.FindByPhoneNumber(phone)
+}
