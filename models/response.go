761a259ca92229cc87fb82d2f31615ab845a09ae
Arya Tabani <a.tabani82@gmail.com>
2025-09-15 17:11:03 +0330

    implementing user and response models

--- models/response.go
+++ models/response.go
@@ -1,1 +1,8 @@
 package models
+
+type APIResponse[T any] struct {
+	Success bool   `json:"success"`
+	Message string `json:"message,omitempty"`
+	Data    T      `json:"data,omitempty"`
+	Error   string `json:"error,omitempty"`
+}
--- models/user.go
+++ models/user.go
@@ -1,1 +1,18 @@
 package models
+
+import "time"
+
+type User struct {
+	ID          int64     `json:"id"`
+	PhoneNumber string    `json:"phone_number"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type RequestOTP struct {
+	PhoneNumber string `json:"phone_number" binding:"required"`
+}
+
+type VerifyOTP struct {
+	PhoneNumber string `json:"phone_number" binding:"required"`
+	OTP         string `json:"otp" binding:"required"`
+}
