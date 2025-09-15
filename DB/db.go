d016ae48143727e99abbd219fdf31edf74790524
Arya Tabani <a.tabani82@gmail.com>
2025-09-15 17:46:41 +0330

    debuging database

--- DB/db.go
+++ DB/db.go
@@ -13,38 +13,33 @@ func InitDB() {
 	var err error
 	DB, err = sql.Open("sqlite3", "otp-service.db")
 	if err != nil {
-		log.Fatalf("Could not connect to database: %v", err)
+		log.Fatalf("could not connect to database: %v", err)
 	}
-
-	if err = DB.Ping(); err != nil {
-		log.Fatalf("Could not ping database: %v", err)
-	}
-
 	createTables()
 }
 
 func createTables() {
-	createUsersTable := `
+	createuserstable := `
 	CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		phone_number TEXT NOT NULL UNIQUE,
 		created_at TIMESTAMP NOT NULL
 	);`
 
-	_, err := DB.Exec(createUsersTable)
+	_, err := DB.Exec(createuserstable)
 	if err != nil {
-		log.Fatalf("Failed to create users table: %v", err)
+		log.Fatalf("failed to create users table: %v", err)
 	}
 
-	createOtpsTable := `
+	createotpstable := `
 	CREATE TABLE IF NOT EXISTS otps (
 		phone_number TEXT PRIMARY KEY,
 		code TEXT NOT NULL,
 		expires_at TIMESTAMP NOT NULL
 	);`
 
-	_, err = DB.Exec(createOtpsTable)
+	_, err = DB.Exec(createotpstable)
 	if err != nil {
-		log.Fatalf("Failed to create otps table: %v", err)
+		log.Fatalf("failed to create otps table: %v", err)
 	}
 }
